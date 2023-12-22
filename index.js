6ce223495477c688e286a3dfb89d831c89c6b48d
Mohammed Salman <144619480+msbotdeveloper@users.noreply.github.com>
2023-12-22 21:51:04 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+
+function copyEmail() {
+  let inputElement = "salman.mohammed.vlr@gmail.com";
+
+  let notification = document.getElementById("notification");
+  navigator.clipboard.writeText(inputElement);
+  notification.innerText = "Email copied";
+  notification.style.color = "white";
+
+  // Get the mouse position
+  var mouseX = event.clientX;
+  var mouseY = event.clientY;
+
+  // Set the position of the notification near the cursor
+  notification.style.left = mouseX + 20 + "px"; // Adjust for spacing
+  notification.style.top = mouseY + "px";
+  notification.style.display = "block";
+
+  // Hide the notification after 2 seconds (adjust as needed)
+  setTimeout(function () {
+    notification.style.display = "none";
+  }, 500);
+}
+
+// Attach the copyEmail function to the input element click event
+document.getElementById("input").addEventListener("click", copyEmail);
+
+const cards = document.querySelectorAll('.card');
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('animation-active');
+    }
+  });
+});
+cards.forEach(card => observer.observe(card));
+
+window.addEventListener('scroll', () => {
+  const viewportHeight = window.innerHeight;
+  cards.forEach(card => {
+    const cardTop = card.offsetTop;
+    if (cardTop < viewportHeight + window.scrollY) {
+      card.classList.add('animation-active');
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
